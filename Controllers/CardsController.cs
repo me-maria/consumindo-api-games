42a9e5a6720a999a472e466645e9bfe39c7cc570
Maria Eduarda <mariaeduardacorrea.mg@gmail.com>
2023-07-17 10:11:12 -0300

    adicionando controller Cards e dependencias

---
+++ Controllers/CardsController.cs
@@ -0,0 +1,51 @@
+﻿using ConsumindoAPIDeGames.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Globalization;
+using X.PagedList;
+
+namespace ConsumindoAPIDeGames.Controllers
+{
+    public class CardsController : Controller
+    {
+        private readonly string ENDPOINT = "https://api.magicthegathering.io/v1/cards";
+        private readonly HttpClient _httpClient = null;
+        private List<Card> _cards = null;
+
+        public CardsController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(ENDPOINT);
+        }
+        public async  Task<IActionResult> Index()
+        {
+            MagicCardsViewModel cardList = null;
+            _cards = new List<Card>();
+            try
+            {
+                var response = await _httpClient.GetAsync(ENDPOINT);
+
+                if (response.IsSuccessStatusCode) {
+                    string content = await response.Content.ReadAsStringAsync();
+                    cardList = JsonConvert.DeserializeObject<MagicCardsViewModel>(content);
+
+                    foreach(var card in cardList.cards)
+                    {
+                        _cards.Add(card);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(null, "Erro ao processar o Magic!");
+                }
+            }
+            catch (Exception ex)
+            {
+                String msg = ex.Message;
+                throw;
+            }
+            return View(cardList.cards);
+        }
+    }
+}
--- Models/MagicCardsViewModel.cs
+++ Models/MagicCardsViewModel.cs
@@ -1,6 +1,67 @@
-﻿namespace ConsumindoAPIDeGames.Models
+﻿namespace ConsumindoAPIDeGames.Model
 {
-    public class MagicCardsViewModels
+    public class MagicCardsViewModel
     {
+        public List<Card> cards { get; set; }
     }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Card
+    {
+        public string name { get; set; }
+        public string manaCost { get; set; }
+        public double cmc { get; set; }
+        public List<string> colors { get; set; }
+        public List<string> colorIdentity { get; set; }
+        public string type { get; set; }
+        public List<string> types { get; set; }
+        public List<string> subtypes { get; set; }
+        public string rarity { get; set; }
+        public string set { get; set; }
+        public string setName { get; set; }
+        public string text { get; set; }
+        public string artist { get; set; }
+        public string number { get; set; }
+        public string power { get; set; }
+        public string toughness { get; set; }
+        public string layout { get; set; }
+        public string multiverseid { get; set; }
+        public string imageUrl { get; set; }
+        public List<string> variations { get; set; }
+        public List<ForeignName> foreignNames { get; set; }
+        public List<string> printings { get; set; }
+        public string originalText { get; set; }
+        public string originalType { get; set; }
+        public List<Legality> legalities { get; set; }
+        public string id { get; set; }
+        public string flavor { get; set; }
+        public List<Ruling> rulings { get; set; }
+        public List<string> supertypes { get; set; }
+    }
+
+    public class ForeignName
+    {
+        public string name { get; set; }
+        public string text { get; set; }
+        public string type { get; set; }
+        public string flavor { get; set; }
+        public string imageUrl { get; set; }
+        public string language { get; set; }
+        public int multiverseid { get; set; }
+    }
+
+    public class Legality
+    {
+        public string format { get; set; }
+        public string legality { get; set; }
+    }
+
+
+    public class Ruling
+    {
+        public string date { get; set; }
+        public string text { get; set; }
+    }
+
+
 }
